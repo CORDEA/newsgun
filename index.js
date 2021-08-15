8259cf3b03a43b20738e673acb60926887a96257
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-15 10:45:43 +0900

    send mail

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const fetch = require('node-fetch');
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
 
 const newsApiToken = process.env.NEWS_API_TOKEN;
 const query = process.env.NEWS_API_QUERY;
+
+const mailgunApiToken = process.env.MAILGUN_API_TOKEN;
+const mailgunDomain = process.env.MAILGUN_DOMAIN;
+const mailgunTo = process.env.MAILGUN_TO;
 
 async function fetchNews() {
     const url = `https://newsapi.org/v2/everything?apiKey=${newsApiToken}&q=${query}&language=en&pageSize=10`;
@@ -21,5 +27,22 @@ function formatJson(json) {
     return body;
 }
 
+async function sendMail(body) {
+    const mailgun = new Mailgun(formData);
+    const client = mailgun.client({username: 'api', key: mailgunApiToken});
+
+    const response = await client.messages.create(
+        mailgunDomain,
+        {
+            from: `mailgun@${mailgunDomain}`,
+            to: [mailgunTo],
+            subject: `News Update: ${query}`,
+            text: body,
+        }
+    );
+    console.log(response);
+}
+
 fetchNews()
-    .then(json => formatJson(json));
+    .then(json => formatJson(json))
+    .then(s => sendMail(s));
